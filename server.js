0822beeb19e062dfa623c5d019adb11a0c363c2e
Rajvardhan <rajthakur3619@gmail.com>
2020-08-09 00:44:38 +0530

    port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const io = require('socket.io')(server)
 const {v4: uuidv4} = require('uuid');
 
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const {ExpressPeerServer} = require('peer');
 const peerServer = ExpressPeerServer(server, {
@@ -38,4 +38,4 @@ io.on('connection', socket => {
 })
 
 
-server.listen('3030')
+server.listen(PORT)
